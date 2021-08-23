f3b156c12deb8e497d1924878861809a6b9cb56f
Cristian Escamilla - c0e027c <escamilla.cristian@gmail.com>
2021-08-22 22:46:39 -0500

    Finish Testing

--- src/test/java/com/example/clip/controller/TransactionControllerTest.java
+++ src/test/java/com/example/clip/controller/TransactionControllerTest.java
@@ -6,19 +6,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.clip.model.dto.DisbursementDto;
@@ -31,7 +29,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(TransactionController.class)
 public class TransactionControllerTest {
 
@@ -51,29 +49,15 @@ public class TransactionControllerTest {
 	private static final String REPORT_ENDPOINT = "/api/clip/report/1";
 	private static final String PAYMENT_ENDPOINT = "/api/clip/createPayload";
 	
-	@BeforeClass
-	public static void init() throws IOException {
 		
-		paymentDto = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentDto.json").toFile(),
-				new TypeReference<PaymentDto>() {
+	@Test
+	public void getUsersWithPaymentsTest() throws Exception {
+		usersDto = new ObjectMapper().readValue(Paths.get("src/test/resources/usersWithPayments.json").toFile(),
+				new TypeReference<List<UserDto>>() {
 				});
 		usersDto = new ObjectMapper().readValue(Paths.get("src/test/resources/usersWithPayments.json").toFile(),
 				new TypeReference<List<UserDto>>() {
 				});
-		disbursmentsDto = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursement.json").toFile(),
-				new TypeReference<List<DisbursementDto>>() {
-				});
-		reportDto = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursement.json").toFile(),
-				new TypeReference<ReportDto>() {
-				});
-		paymentRequest = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentRequest.json").toFile(),
-				new TypeReference<PaymentRequest>() {
-				});
-				
-	}
-	
-	@Test
-	public void getUsersWithPaymentsTest() throws Exception {
 		doReturn(usersDto).when(service).getUserWithPayment();
 		this.mockMvc.perform(get(USERS_WITH_PAYMENTS_ENDPOINT).accept(MediaType.APPLICATION_JSON))
 		     .andDo(print()).andExpect(status().isOk());
@@ -82,6 +66,9 @@ public class TransactionControllerTest {
 	
 	@Test
 	public void processDisbursementTest() throws Exception {
+		disbursmentsDto = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursement.json").toFile(),
+				new TypeReference<List<DisbursementDto>>() {
+				});
 		doReturn(disbursmentsDto).when(service).disbursementProcess();
 		this.mockMvc.perform(post(DISBURSEMENT_PROCESS_ENDPOINT).accept(MediaType.APPLICATION_JSON))
 		.andDo(print()).andExpect(status().isCreated());
@@ -89,6 +76,9 @@ public class TransactionControllerTest {
 	
 	@Test
 	public void getReportTest() throws Exception{
+		reportDto = new ObjectMapper().readValue(Paths.get("src/test/resources/report.json").toFile(),
+				new TypeReference<ReportDto>() {
+				});
 		doReturn(reportDto).when(service).getReportByIdUser(ArgumentMatchers.eq(1L));
 		this.mockMvc.perform(get(REPORT_ENDPOINT).accept(MediaType.APPLICATION_JSON))
 		.andDo(print()).andExpect(status().isOk());
@@ -96,6 +86,12 @@ public class TransactionControllerTest {
 	
 	@Test
 	public void createPayLoadTest() throws JsonProcessingException, Exception{
+		paymentDto = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentDto.json").toFile(),
+				new TypeReference<PaymentDto>() {
+				});
+		paymentRequest = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentRequest.json").toFile(),
+				new TypeReference<PaymentRequest>() {
+				});
 		doReturn(paymentDto).when(service).create(paymentRequest);
 		this.mockMvc.perform(post(PAYMENT_ENDPOINT).contentType(MediaType.APPLICATION_JSON)
 				.content(new ObjectMapper().writeValueAsString(paymentRequest)).accept(MediaType.APPLICATION_JSON))
--- src/test/java/com/example/clip/service/TransactionServiceTest.java
+++ src/test/java/com/example/clip/service/TransactionServiceTest.java
@@ -1,19 +1,24 @@
 package com.example.clip.service;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
-
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import com.example.clip.config.ClipConfiguration;
@@ -21,7 +26,10 @@ import com.example.clip.model.dto.DisbursementDto;
 import com.example.clip.model.dto.PaymentDto;
 import com.example.clip.model.dto.ReportDto;
 import com.example.clip.model.dto.UserDto;
+import com.example.clip.model.entities.Disbursement;
 import com.example.clip.model.entities.Payment;
+import com.example.clip.model.entities.User;
+import com.example.clip.model.enums.PaymentStatus;
 import com.example.clip.repository.DisbursementRepository;
 import com.example.clip.repository.PaymentRepository;
 import com.example.clip.repository.UserRepository;
@@ -29,60 +37,84 @@ import com.example.clip.request.PaymentRequest;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
 
 	@Mock
 	PaymentRepository paymentRepository;
-	
+
 	@Mock
 	UserRepository userRepository;
-	
+
 	@Mock
 	DisbursementRepository disbursementRepository;
-	
+
 	@Mock
 	ClipConfiguration clipConfiguration;
-	
+
 	@Spy
 	ModelMapper mapper;
-	
+
 	@InjectMocks
 	private TransactionService service;
-	
+
 	private static Payment payment;
-	private static List<UserDto> usersDto;
-	private static List<DisbursementDto> disbursmentsDto;
-	private static ReportDto reportDto;
+	private static User user;
+	private static Disbursement disbursement;
 	private static PaymentRequest paymentRequest;
-	
-	@Before
-	public void init() throws IOException{
+
+	@BeforeEach
+	public void init() throws IOException {
 		payment = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentDto.json").toFile(),
 				new TypeReference<Payment>() {
 				});
-		usersDto = new ObjectMapper().readValue(Paths.get("src/test/resources/usersWithPayments.json").toFile(),
-				new TypeReference<List<UserDto>>() {
-				});
-		disbursmentsDto = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursement.json").toFile(),
-				new TypeReference<List<DisbursementDto>>() {
-				});
-		reportDto = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursement.json").toFile(),
-				new TypeReference<ReportDto>() {
-				});
+		disbursement = new ObjectMapper().readValue(Paths.get("src/test/resources/disbursementOnly.json").toFile(),
+				new TypeReference<Disbursement>() {
+		});
 		paymentRequest = new ObjectMapper().readValue(Paths.get("src/test/resources/paymentRequest.json").toFile(),
 				new TypeReference<PaymentRequest>() {
 				});
+		user = new ObjectMapper().readValue(Paths.get("src/test/resources/user.json").toFile(),
+				new TypeReference<User>() {
+				});
 	}
-	
-	
+
 	@Test
 	public void createTest() {
-		when(paymentRepository.save(payment)).thenReturn(payment);
-		PaymentDto paymendto = service.create(paymentRequest);
-		assertNotNull(paymendto);
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+		PaymentDto paymentdto = service.create(paymentRequest);
+		assertNotNull(paymentdto);
+		assertEquals(BigDecimal.valueOf(1000), paymentdto.getAmount());
+	}
+
+	@Test
+	public void getUserWithPaymentTest() {
+		when(paymentRepository.findDistinctUserId()).thenReturn(Lists.newArrayList(1L));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		List<UserDto> result = service.getUserWithPayment();
+		assertNotNull(result);
+		assertEquals(1, result.size());
+
 	}
 	
+	@Test
+	public void disbursementProcessTest() {
+		when(paymentRepository.findByStatus(PaymentStatus.NEW.name())).thenReturn(Lists.newArrayList(payment));
+		when(disbursementRepository.save(any(Disbursement.class))).thenReturn(disbursement);
+		when(clipConfiguration.getDisbursmentFee()).thenReturn(BigDecimal.valueOf(1.035));
+		List<DisbursementDto> result = service.disbursementProcess();
+		assertNotNull(result);
+		assertEquals(1, result.size());
+	}
 	
-	
+	@Test
+	public void reportByIdUserTest() {
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		when(paymentRepository.findByUserId(1L)).thenReturn(Lists.newArrayList(payment));
+		ReportDto result = service.getReportByIdUser(1L);
+		assertNotNull(result);
+		assertEquals(1, result.getPayments_sum());
+	}
+
 }
